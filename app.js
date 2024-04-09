bf8b62120ea58a913e2437bb82970c3b6d48e10f
Anjan11 <anjan.nagilla@yahoo.com>
2024-04-09 14:19:10 +0000

    Pushing the updated content

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Youtube!"));
+app.get("/", (req, res) => res.send("This docker nodejs container app!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
